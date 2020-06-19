04ba9f792ce3c4c58fbf3e17cfca6387ff05587c
miguel navarrete <mnavarretem25@gmail.com>
2020-06-19 04:47:54 -0400

    version 1.1
    presentacion de servicio

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('America/Santiago');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://codeigniter-crud.loc/';
+$config['base_url'] = 'http://localhost/tutorial_login-master/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/header.php
+++ application/views/header.php
@@ -46,7 +46,7 @@
 	?>
 
 	<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-	  <a class="navbar-brand" href="#">Navbar</a>
+	  <a class="navbar-brand" href="#">INCA</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 		aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
