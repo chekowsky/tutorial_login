0b402c826e5f527b31140b364ce2c41efabf9c7d
miguel navarrete <mnavarretem25@gmail.com>
2020-06-19 03:55:28 -0400

    cambio completo de accion

--- application/views/view_usuarios.php
+++ application/views/view_usuarios.php
@@ -26,21 +26,21 @@
 <div class="container">
 	<h2 align="center">Catalogo de Usuarios</h2>
 	<?php
-if(isset($_GET['save'])){
-	echo '<div class="alert alert-success text-center">La Información  se Almaceno Correctamente</div>';
+if (isset($_GET['save'])) {
+    echo '<div class="alert alert-success text-center">La Información  se Almaceno Correctamente</div>';
 }
-if(isset($_GET['delete'])){
-	echo '<div class="alert alert-warning text-center">La Información  se ha Eliminado Correctamente</div>';
+if (isset($_GET['delete'])) {
+    echo '<div class="alert alert-warning text-center">La Información  se ha Eliminado Correctamente</div>';
 }
-if(isset($_GET['update'])){
-	echo '<div class="alert alert-success text-center">La Información  se Actualizo Correctamente</div>';
+if (isset($_GET['update'])) {
+    echo '<div class="alert alert-success text-center">La Información  se Actualizo Correctamente</div>';
 }
-if(isset($_GET['permisos'])){
-		echo '<div class="alert alert-success text-center">Los Permisos fueron Asignados Correctamente</div>';
-	}
-	if(isset($_GET['password'])){
-		echo '<div class="alert alert-success text-center">La Contraseña fue actualizado Correctamente</div>';
-	}
+if (isset($_GET['permisos'])) {
+    echo '<div class="alert alert-success text-center">Los Permisos fueron Asignados Correctamente</div>';
+}
+if (isset($_GET['password'])) {
+    echo '<div class="alert alert-success text-center">La Contraseña fue actualizado Correctamente</div>';
+}
 ?>
 <table id="usuarios" cellspacing="0" width="auto">
 	<thead>
@@ -56,39 +56,42 @@ if(isset($_GET['permisos'])){
 		</tr>
 	</thead>
 <tbody>
- <?php 
- $contador = 0;
- if(!empty($usuarios)){
- 	foreach($usuarios as $usuario){
- 		echo '<tr>';
- 		echo '<td>'.$usuario->NOMBRE.'</td>';
-		echo '<td>'.$usuario->APELLIDOS.'</td>';
-		echo '<td>'.$usuario->ANEXO.'</td>';
-		echo '<td>'.$usuario->EMAIL.'</td>';
-		echo '<td>'.$usuario->FECHA_REGISTRO.'</td>';
-		echo '<td>'.$usuario->TIPO.'</td>';
- 			
- 			/*Si es estatus mostramos en texto*/
-			if($usuario->TIPO == 1){
-			echo '<td>Activo</td>';
-			}
-			if($usuario->TIPO == 0){
-			echo '<td>Inactivo</td>';
-			}
-		echo '<td>'
-		?>
-		<a href="<?php echo base_url();?>index.php/usuarios/editar/<?php echo $usuario->ID;?>/" class="btn btn-success">Editar</a>
-		<a href="<?php echo base_url();?>index.php/usuarios/password/<?php echo $usuario->ID ?>" class="btn btn-default">Password</a>
-		<a href="<?php echo base_url();?>index.php/usuarios/permisos/<?php echo $usuario->ID;?>" class="btn btn-info">Permisos</a>
-		<a href="<?php echo base_url();?>index.php/usuarios/eliminar/<?php echo $usuario->ID ?>" class="btn btn-danger">Eliminar</a>
-		<?php
-		echo '</td>';
+ <?php
 
- 		echo '</tr>';
- 	} 
- }
+if (!empty($usuarios)) {
+    foreach ($usuarios as $usuario) {
+        echo '<tr>';
+        echo '<td>' . $usuario->NOMBRE . '</td>';
+        echo '<td>' . $usuario->APELLIDOS . '</td>';
+        echo '<td>' . $usuario->ANEXO . '</td>';
+        echo '<td>' . $usuario->EMAIL . '</td>';
+        echo '<td>' . $usuario->FECHA_REGISTRO . '</td>';
+        echo '<td>' . $usuario->TIPO . '</td>';
 
- ?>
+        /*Si es estatus mostramos en texto*/
+        if ($usuario->ESTATUS == 1) {
+            echo '<td>Activo</td>';
+        }
+        if ($usuario->ESTATUS == 0) {
+            echo '<td>Inactivo</td>';
+        }
+        echo '<td>';
+
+        if ($this->session->userdata('TIPOUSUARIO') == 'Administrador') {
+            echo '<a href="' . base_url() . 'index.php/usuarios/editar/' . $usuario->ID . '" class="btn btn-success">Editar</a>';
+            echo '<a href="' . base_url() . 'index.php/usuarios/password/' . $usuario->ID . '" class="btn btn-default">Password</a>';
+            echo '<a href="' . base_url() . 'index.php/usuarios/permisos/' . $usuario->ID . '" class="btn btn-info">Permisos</a>';
+            echo '<a href="' . base_url() . 'index.php/usuarios/eliminar/' . $usuario->ID . '" class="btn btn-danger">Eliminar</a>';
+
+        }
+
+        echo '</td>';
+
+        echo '</tr>';
+    }
+}
+
+?>
 </tbody>
 </table>
 
